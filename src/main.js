1d965cc0687ac9980c00605639e974c231341944
Vivi <vachetvirginie@gmail.com>
2020-09-15 10:25:33 +0200

    add calendar vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
+
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import firebase from "firebase";
+import VueTextareaAutosize from 'vue-textarea-autosize'
+
+Vue.use(VueTextareaAutosize)
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: "AIzaSyB51cuf-QqUtA0BQNjBevF9KZr_ceAeO48",
+  authDomain: "",
+  databaseURL: "https://waspycalendar.firebaseio.com",
+  projectId: "waspycalendar",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+});
+
+export const db = firebase.firestore();
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
